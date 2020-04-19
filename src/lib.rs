90783e3ab77045c311201536f7cfa8b1703ff9d2
Martin <mato.stefko@gmail.com>
2020-04-19 20:44:18 +0200

    Added rust new(),subtract(), scale_by() in Vector constructor, Game consctructor with new(), variables and instance return

--- src/lib.rs
+++ src/lib.rs
@@ -33,7 +33,7 @@ pub struct Game{
     pub width: i32,
     pub height: i32,
     // some type for measuring time
-    pub speed: f64;
+    pub speed: f64,
     pub score: i32,
     pub direction: Vector,
     pub food: Vector,
@@ -41,12 +41,53 @@ pub struct Game{
     snake:Vec<Vector>
 }
 
+// constructor of vector struct
+#[wasm_bindgen]
+impl Vector{
+    #[wasm_bindgen(constructor)]
+    pub fn new(x:f64,y:f64)-> Vector{
+        Vector{x,y}
+    }
+    // subtract returns a reference to another Vector
+    pub fn subtract(&self,other: &Vector)-> Vector {
+        // create new instance of a Vector where x is self.x-other.x,
+        // where other is &Vector returned vector? 
+        Vector::new(self.x - other.x,self.y - other.y)
+    }
+    //  create new vector 
+    pub fn scale_by(&self,number:f64) -> Vector {
+        Vector::new(self.x*number,self.y*number)
+    }
+}
+
 // this is constructor for the Game
+#[wasm_bindgen]
 impl Game{
+    #[wasm_bindgen(constructor)]
     pub fn new(width: i32,height:i32,speed:f64,snake_length:i32,direction:Vector)-> Game{
         // divide width/2 round() it and substract half of the cell adn assign it to head_x
         let head_x = (f64::from(width)/2_f64).round()-0.5;
         let head_y = (f64::from(height)/2_f64).round()-0.5;
-        let head = Vector::new(head_x,head_y)
+        let head = Vector::new(head_x,head_y);
+        // subtracting direction scaled by snake_length from head
+        let tailtip = head.subtract(&direction.scale_by(f64::from(snake_length)));
+        // snake will be a vector of tailtip and head
+        let snake = vec![tailtip,head];
+        // food will be half of the cells located in the first cell
+        // TODO: place foo in random cells
+        let food = Vector::new(0.5,0.5);
+
+        // Returning the instance of the game struct
+        Game {
+            width: width,
+            height: height,
+            // number of cells snake cross in one second
+            speed: speed,
+            snake: snake,
+            direction: direction,
+            food: food,
+            score: 0
+        }
+
     }
 }
